a5a11a99141326c6d94d659a63564085f39c85c1
dpoulimenos <c0029123@newcastle.ac.uk>
2020-10-26 12:07:19 +0000

    Added the no_rotate argument and finish the code!

--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -1,11 +1,14 @@
 from math import pi, sin, cos
 
+
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 
-class MyApp(ShowBase):
-    def __init__(self):
+
+class WalkingPanda(ShowBase):
+
+    def __init__(self, no_rotate=False):
         ShowBase.__init__(self)
 
         # Load the environment model.
@@ -16,16 +19,19 @@ class MyApp(ShowBase):
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
 
-        # Add the spinCameraTask procedure to the task manager.
-        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+        if not no_rotate:
+            # Add the spinCameraTask procedure to the task manager.
+            self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
 
         # Load and transform the panda actor.
         self.pandaActor = Actor("models/panda-model",
-                                 {"walk": "models/panda-walk4"})
+                                    {"walk": "models/panda-walk4"})
         self.pandaActor.setScale(0.005, 0.005, 0.005)
         self.pandaActor.reparentTo(self.render)
         # Loop its animation.
         self.pandaActor.loop("walk")
+
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
@@ -34,6 +40,3 @@ class MyApp(ShowBase):
         self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
         self.camera.setHpr(angleDegrees, 0, 0)
         return Task.cont
-
-app = MyApp()
-app.run()
